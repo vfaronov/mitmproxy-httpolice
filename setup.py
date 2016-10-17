7a37f6ac96a2462e3425dc30dff8a50506964244
Vasiliy Faronov <vfaronov@gmail.com>
2016-10-18 01:14:21 +0300

    Python 3 in trove classifiers

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Quality Assurance',
     ],
